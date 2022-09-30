f0e8053aff76bad8fd09a37954cb6832a1889668
unknown <bariskaftancioglu@gmail.com>
2022-09-30 14:13:18 +0200

    2 First function

--- Analyse.py
+++ Analyse.py
@@ -8,17 +8,20 @@ Created on Fri Sep 30 13:46:06 2022
 def decodable_offset(offset):
 	if len(offset) < 2 or not offset.isalnum():
 		return False
-	for chiffre_hex in offset:
-		if chiffre_hex.isalpha():
-			if not(chiffre_hex.lower() >= "a" and chiffre_hex.lower() <= "f"):
+	for chiffre_hexadecimal in offset:
+		if chiffre_hexadecimal.isalpha():
+			if not(chiffre_hexadecimal.lower() in ["a","b","c","d","e","f"]):
 				return False
 	return True
 	
 def decodable_byte(byte):
 	if len(byte) != 2 or not byte.isalnum():
 		return False
-	for chiffre_hex in byte:
-		if chiffre_hex.isalpha():
-			if not(chiffre_hex.lower() >= "a" and chiffre_hex.lower() <= "f"):
+	for chiffre_hexadecimal in byte:
+		if chiffre_hexadecimal.isalpha():
+			if not(chiffre_hexadecimal.lower() in ["a","b","c","d","e","f"] ):
 				return False
 	return True
+
+
+print(decodable_offset("D"))
