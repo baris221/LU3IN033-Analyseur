5420d9c9668b7cc3a831f6bf8655f9cfab8c6ff9
unknown <bariskaftancioglu@gmail.com>
2022-10-06 20:31:16 +0200

    Des commentaires

--- Analyse.py
+++ Analyse.py
@@ -27,11 +27,15 @@ def decodable_byte(byte):
 def lire_trace(path_to_file):
 	with open(path_to_file, "rt") as f:
 		frame_list = []
+        #line_list contient les contenus de la liste
 		line_list = f.read().splitlines()
+        #L'offset doit commencer par 0
 		if int(line_list[0].split()[0], 16) != 0:
 			print("Error: Malformed frame: Frame doesn't start with offset 0")
 			return
 		i = -1
+        #Frame list va contenir des lignes qui commencent par des binaires decodables
+        #Si ça commence par zéro,on le met en avant sinon à la fin
 		for j in range(len(line_list)):
 			line_list[j] = line_list[j].split()
 			if decodable_offset(line_list[j][0]):
@@ -39,7 +43,8 @@ def lire_trace(path_to_file):
 					i = i+1
 					frame_list.append([])
 			frame_list[i].append(line_list[j])
-			
+
+        #On enléve si le binaire non décodable de frame			
 		for frame in frame_list:
 			for line in frame:
 				if not decodable_offset(line[0]):
@@ -48,13 +53,17 @@ def lire_trace(path_to_file):
 		#v2
 		for frame in frame_list:
 			last_offset = -1
+            #On prend l'identifiant de chaque élement de frame
 			for idx, line in enumerate(frame):
 				if idx != len(frame)-1:
 					#print(str(int(line[0], 16)) + ">" + str(last_offset))
+                    #Si la valeur de l'octet est plus grand que -1
 					if int(line[0], 16) > last_offset:
 						next_offset = frame[(idx + 1) % len(frame)][0]
 						#print(next_offset)
+                        #Revalorisation de last_offset
 						last_offset = int(line[0], 16)
+                        #Si la valeur de offset prochain est différent de 0.
 						if int(next_offset, 16) > 0:
 							if ((len(line) - 1) == int(next_offset, 16) - int(line[0], 16)):
 								line.pop(0)
@@ -65,6 +74,7 @@ def lire_trace(path_to_file):
 								nb_bytes =  int(next_offset, 16) - int(line[0], 16)
 								line.pop(0)
 								del line [nb_bytes:]
+                #dernier octet
 				else:
 					line.pop(0)
 					for i in range(len(line)):
@@ -77,5 +87,7 @@ def lire_trace(path_to_file):
 							
 		
 		
-		#print(frame_list)
+		print(frame_list)
 		return frame_list
+    
+li=lire_trace("deux-trames-correctes.trame")
