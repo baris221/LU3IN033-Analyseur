0ee8728a4434205bccc5d4e7abe0d6af4d724c3d
unknown <bariskaftancioglu@gmail.com>
2022-12-02 13:28:05 +0100

    Maintenant tout est parfait

--- http.py
+++ http.py
@@ -9,7 +9,9 @@ def http_decoder(list_octets):
     
     hex_string = ''.join(str(x) for x in list_octets) # On transforme la liste d'octets en une chaine de caractères
     http_string = "Hypertext Transfer Protocol\n" # La chaine de caractère qui contiendra le message HTTP
-    
+    numeroligne=0
+    httpAmontrer=""
+
     last_index = 0 # L'index de chaque début de ligne
     for index in range(0, len(hex_string)):
         temporary_string = ''
@@ -18,7 +20,12 @@ def http_decoder(list_octets):
             temporary_string = hex_string[last_index:index + 4] 
             http_string = http_string + "\t" + bytes.fromhex(temporary_string).decode('ASCII')
             last_index = index + 4
+            numeroligne=numeroligne+1
+            print(numeroligne)
+            if(numeroligne==1):
+                httpAmontrer=bytes.fromhex(temporary_string).decode('ASCII')
+                print(httpAmontrer)
         # Condition de fin d'entête HTTP
         if(hex_string[index] == '0' and hex_string[index+1] == 'd' and hex_string[index+2] == '0' and hex_string[index+3] == 'a' and hex_string[index+4] == '0' and hex_string[index+5] == 'd' and hex_string[index+6] == '0' and hex_string[index+7] == 'a'):
             print(http_string) # On affiche le message HTTP
-            return (list_octets[int((index+8)/2):],http_string[29:49])
+            return (list_octets[int((index+8)/2):],httpAmontrer)
